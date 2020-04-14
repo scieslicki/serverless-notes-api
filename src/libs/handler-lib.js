b6e1b3e529444a4e9a129cd95006dd16b3a63913
Staszek Cieslicki <s.cieslicki@clearcode.cc>
2020-04-14 11:27:14 +0200

    Adding error logging

--- src/libs/handler-lib.js
+++ src/libs/handler-lib.js
@@ -1,9 +1,13 @@
+import * as debug from "./debug-lib";
+
 export default function handler(lambda) {
   return function (event, context) {
     return Promise.resolve()
+      .then(() => debug.init(event, context))
       .then(() => lambda(event, context))
       .then((responseBody) => [200, responseBody])
       .catch((e) => {
+        debug.flush(e);
         return [500, { error: e.message }];
       })
       .then(([statusCode, body]) => ({
@@ -13,7 +17,8 @@ export default function handler(lambda) {
           "Access-Control-Allow-Credentials": true
         },
         body: JSON.stringify(body),
-      }));
+      }))
+      .finally(debug.end);
   };
 
 }
